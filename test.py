74976c1450b2be98bf515750e510bef4282f1c44
Christopher-Guarino1 <Christopher.Guarino1@ibm.com>
2025-12-26 18:57:15 -0500

    2nd commit

--- test.py
+++ test.py
@@ -3,15 +3,19 @@ import cv2
 
 tello = Tello()
 tello.connect()
+
+print("Battery:", tello.get_battery(), "%")
+
 tello.streamon()
 
 while True:
     frame = tello.get_frame_read().frame
     frame = cv2.resize(frame, (640, 480))
-    cv2.imshow("Tello", frame)
+    cv2.imshow("Tello Camera", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 tello.streamoff()
-tello.land()
+cv2.destroyAllWindows()
+tello.end()
